ce4f10054cfa735f8de363823bc48ef780069ade
Steven Hopkins <steven.hopkins@vgs.io>
2018-12-06 17:24:47 -0700

    fixing jsonpath-rw req

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ VERSION = None
 # What packages are required for this module to be executed?
 REQUIRED = [
     'pyyaml', 
-    'jsonpath'
+    'jsonpath-rw>=1.4.0'
 ]
 
 # What packages are optional?
