0a488979dc84eab2e34ed195a088302b8ec6e56c
Steven Hopkins <steven.hopkins@vgs.io>
2018-12-18 10:09:58 -0700

    Bump version: 0.4.1 → 0.4.2

--- jsonrouter/__version__.py
+++ jsonrouter/__version__.py
@@ -1,6 +1,6 @@
 # This is to get around bumpversion issue and still use a tuple of int
 # https://github.com/peritus/bumpversion/issues/25
-VERSION = tuple(int(i) for i in '0.4.1'.split('.'))
+VERSION = tuple(int(i) for i in '0.4.2'.split('.'))
 
 __version_info__ = VERSION
 __version__ = '.'.join(map(str, VERSION))
